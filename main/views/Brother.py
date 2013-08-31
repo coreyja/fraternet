1f14bfb78d701944560ef9eaf2686819552fb269
Corey <coreyja@gmail.com>
2013-08-31 16:42:03 -0400

    Added Permission and Login required to views

--- main/views/Brother.py
+++ main/views/Brother.py
@@ -2,6 +2,10 @@ from django.views.generic import DetailView, UpdateView, CreateView, DeleteView,
 from django.shortcuts import render_to_response, redirect
 from django.core.exceptions import PermissionDenied
 
+from django.contrib.auth.decorators import login_required, permission_required
+from django.utils.decorators import method_decorator
+
+
 from main.models import Brother
 from main.forms import BrotherForm, BrotherCreateForm
 
@@ -13,6 +17,10 @@ class BrotherCreateView(FormView):
     template_name = 'brother/create.html'
     form_class = BrotherCreateForm
     success_url = '/'
+
+    @method_decorator(permission_required('main.add_brother'))
+    def dispatch(self, *args, **kwargs):
+        return super(BrotherCreateView, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -45,9 +53,17 @@ class BrotherListView(ListView):
     model = Brother
     template_name = 'brother/list.html'
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(BrotherListView, self).dispatch(*args, **kwargs)
+
 class BrotherDetailView(DetailView):
     model = Brother
     template_name = 'brother/detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(BrotherDetailView, self).dispatch(*args, **kwargs)
 
     def get_object(self, queryset=None):
         return Brother.objects.get(username=self.kwargs['username'])
