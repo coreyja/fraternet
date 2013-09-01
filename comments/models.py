f6837683c0ede13f58bda61e4777388887b82cb2
Corey <coreyja@gmail.com>
2013-09-01 14:44:29 -0400

    Added simple displaying of comments to Rushie Detail Page

--- comments/models.py
+++ comments/models.py
@@ -9,3 +9,6 @@ class Comment(models.Model):
     comment = models.TextField()
 
     created_by = models.ForeignKey(Profile, related_name='comments_made')
+
+    def __unicode__(self):
+        return self.comment
