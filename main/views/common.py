e476c1c7d0fcf5a3cc7358baf184ca2f1945e44c
Corey <coreyja@gmail.com>
2013-09-04 15:33:42 -0400

    Homepage Redirects to Calendar

--- main/views/common.py
+++ main/views/common.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.views.generic import DetailView, UpdateView
 from django.contrib.auth.decorators import login_required, permission_required
@@ -12,10 +13,12 @@ from rush.models import Rushie
 from rush.forms import RushieEditForm
 
 def dashboard_view(request):
-    if request.user.is_authenticated():
-        return render_to_response('dashboard.html', context_instance=RequestContext(request))
-    else:
-        return render_to_response('static/landing.html', context_instance=RequestContext(request))
+    return redirect('calendar_view')
+
+    # if request.user.is_authenticated():
+    #     return render_to_response('dashboard.html', context_instance=RequestContext(request))
+    # else:
+    #     return render_to_response('static/landing.html', context_instance=RequestContext(request))
 
 
 class ProfileView(DetailView):
